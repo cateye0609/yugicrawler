7dff4821c86e1acb19559b6bbcdf0c20cd5b0ffb
Quang Dong <magenkyou0609@gmail.com>
2022-10-30 15:58:51 +0700

    feat: add normal category filter

--- api/utils.js
+++ api/utils.js
@@ -1,4 +1,4 @@
 export function getMonsterType(types) {
-	const monsterType = ['fusion', 'synchro', 'xyz', 'link', 'ritual', 'pendulum'];
+	const monsterType = ['normal', 'fusion', 'synchro', 'xyz', 'link', 'ritual', 'pendulum'];
 	return monsterType.find(e => types.toLowerCase().includes(e));
 }
