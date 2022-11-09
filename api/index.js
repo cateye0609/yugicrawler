ace8014a310993d92d88150a1df563f1a472e7f1
Quang Dong <magenkyou0609@gmail.com>
2022-11-09 23:46:08 +0700

    feat: update for better catch error

--- api/index.js
+++ api/index.js
@@ -64,119 +64,121 @@ app.get('/api/data/:name', cors(corsOptionsDelegate), async (req, res) => {
   const url = `https://yugipedia.com/wiki/${name.replace(" ", "_")}`;
   try {
     request(url, (error, response, html) => {
-      if (response.statusCode !== 200) {
-        console.error("Can't get page: ", response.statusMessage);
-      }
-      const $ = load(html);
+      if (response && response.statusCode === 200) {
+        const $ = load(html);
 
-      const name = $(".card-table > .heading").text().trim();
+        const name = $(".card-table > .heading").text().trim();
 
-      const infoHtml = $(".infocolumn table.innertable tbody");
-      const cardType = infoHtml.find("tr:contains('Card type') > td").text().trim();
-      let cardProperty = { name, cardType };
-      if (cardType === CARD_TYPE.monster) {
-        const monsterTypes = infoHtml.find(`tr:contains('Types') > td`).text().trim();
-        const type = getMonsterType(monsterTypes);
-        let propsList = [];
-        switch (type) {
-          case "xyz":
-            propsList = MONSTER_PROPERTY.xyz;
-            break;
-          case "pendulum":
-            propsList = MONSTER_PROPERTY.pendulum;
-            break
-          case "link":
-            propsList = MONSTER_PROPERTY.link;
-            break;
-          default:
-            propsList = MONSTER_PROPERTY.monster;
-            break;
+        const infoHtml = $(".infocolumn table.innertable tbody");
+        const cardType = infoHtml.find("tr:contains('Card type') > td").text().trim();
+        let cardProperty = { name, cardType };
+        if (cardType === CARD_TYPE.monster) {
+          const monsterTypes = infoHtml.find(`tr:contains('Types') > td`).text().trim();
+          const type = getMonsterType(monsterTypes);
+          let propsList = [];
+          switch (type) {
+            case "xyz":
+              propsList = MONSTER_PROPERTY.xyz;
+              break;
+            case "pendulum":
+              propsList = MONSTER_PROPERTY.pendulum;
+              break
+            case "link":
+              propsList = MONSTER_PROPERTY.link;
+              break;
+            default:
+              propsList = MONSTER_PROPERTY.monster;
+              break;
+          }
+
+          if (type === "link") {
+            const atkLink = infoHtml.find(`tr:contains('ATK / LINK') > td`).text().trim().split("/");
+            const linkArrows = infoHtml.find(`tr:contains('Link Arrows') > td`).text().trim().split(",").map(e => e.trim());
+            cardProperty = {
+              ...cardProperty,
+              atk: atkLink[0].trim(),
+              linkRating: atkLink[1].trim(),
+              linkArrows
+            }
+          } else {
+            const atkDef = infoHtml.find(`tr:contains('ATK / DEF') > td`).text().trim().split("/");
+            cardProperty = {
+              ...cardProperty,
+              atk: atkDef[0].trim(),
+              def: atkDef[1].trim(),
+            }
+          }
+
+          propsList.forEach(prop => {
+            cardProperty = {
+              ...cardProperty,
+              monsterTypes,
+              [prop.toLowerCase().replace(/ /g, "_")]: infoHtml.find(`tr:contains('${prop}') > td`).first().text().trim()
+            }
+          });
+        } else {
+          ST_PROPERTY.forEach(prop => {
+            cardProperty = {
+              ...cardProperty,
+              [prop.toLowerCase()]: infoHtml.find(`tr:contains('${prop}') > td`).text().trim()
+            }
+          });
         }
 
-        if (type === "link") {
-          const atkLink = infoHtml.find(`tr:contains('ATK / LINK') > td`).text().trim().split("/");
-          const linkArrows = infoHtml.find(`tr:contains('Link Arrows') > td`).text().trim().split(",").map(e => e.trim());
-          cardProperty = {
-            ...cardProperty,
-            atk: atkLink[0].trim(),
-            linkRating: atkLink[1].trim(),
-            linkArrows
+        const cardSetHtml = $("table#cts--EN.cts").length
+          ? $("table#cts--EN.cts").find("tbody > tr").toArray()
+          : $("table#cts--JP.cts").find("tbody > tr").toArray();
+        const cardSet = cardSetHtml.slice(1).map(e => $(e).find("td").toArray());
+        const cardSetData = cardSet.map(e => e.map(e2 => $(e2).text().trim()));
+
+        const types = $("table.innertable > tbody > tr:nth-child(3) > td").text();
+        const isPendulum = types.toLowerCase().includes("pendulum");
+        const extraDeckTypes = ['fusion', 'synchro', 'xyz', 'link'];
+        const isExtraMonster = extraDeckTypes.findIndex(e => types.toLowerCase().includes(e)) > -1;
+
+        const effect = isPendulum
+          ? $('.lore dd').toArray().map(e => $(e).find("br").replaceWith("\n").end().text().trim())
+          : $(".lore > p").find("br").replaceWith("\n").end().text().trim().split("\n");
+        let effectResult = null;
+        if (isPendulum) {
+          const monsterEff = effect[1].split("\n");
+          effectResult = {
+            monsterEffect: monsterEff.length > 1 ? `[${monsterEff[0]}]\n${monsterEff.slice(1).join("\n")}` : monsterEff[0],
+            pendulumEffect: effect[0]
           }
         } else {
-          const atkDef = infoHtml.find(`tr:contains('ATK / DEF') > td`).text().trim().split("/");
-          cardProperty = {
-            ...cardProperty,
-            atk: atkDef[0].trim(),
-            def: atkDef[1].trim(),
-          }
+          effectResult = {
+            monsterEffect: effect.length > 1
+              ? (isExtraMonster ? `[${effect[0]}]\n${effect.slice(1).join("\n")}` : effect.join("\n"))
+              : effect[0]
+          };
         }
 
-        propsList.forEach(prop => {
-          cardProperty = {
-            ...cardProperty,
-            monsterTypes,
-            [prop.toLowerCase().replace(/ /g, "_")]: infoHtml.find(`tr:contains('${prop}') > td`).first().text().trim()
-          }
-        });
-      } else {
-        ST_PROPERTY.forEach(prop => {
-          cardProperty = {
-            ...cardProperty,
-            [prop.toLowerCase()]: infoHtml.find(`tr:contains('${prop}') > td`).text().trim()
-          }
-        });
-      }
+        const result = {
+          ...cardProperty,
+          effect: effectResult.monsterEffect,
+          pendulumEffect: effectResult.pendulumEffect,
+          cardSet: cardSetData.map(item => {
+            return {
+              releasedDate: item[0],
+              code: item[1],
+              name: item[2],
+              rarity: item[3],
+            }
+          }),
+          password: cardProperty.password === 'None' ? null : cardProperty.password,
+          isToken: cardProperty.password === 'None' && !!cardProperty.limitation_text
+        }
 
-      const cardSetHtml = $("table#cts--EN.cts").length
-        ? $("table#cts--EN.cts").find("tbody > tr").toArray()
-        : $("table#cts--JP.cts").find("tbody > tr").toArray();
-      const cardSet = cardSetHtml.slice(1).map(e => $(e).find("td").toArray());
-      const cardSetData = cardSet.map(e => e.map(e2 => $(e2).text().trim()));
-
-      const types = $("table.innertable > tbody > tr:nth-child(3) > td").text();
-      const isPendulum = types.toLowerCase().includes("pendulum");
-      const extraDeckTypes = ['fusion', 'synchro', 'xyz', 'link'];
-      const isExtraMonster = extraDeckTypes.findIndex(e => types.toLowerCase().includes(e)) > -1;
-
-      const effect = isPendulum
-        ? $('.lore dd').toArray().map(e => $(e).find("br").replaceWith("\n").end().text().trim())
-        : $(".lore > p").find("br").replaceWith("\n").end().text().trim().split("\n");
-      let effectResult = null;
-      if (isPendulum) {
-        const monsterEff = effect[1].split("\n");
-        effectResult = {
-          monsterEffect: monsterEff.length > 1 ? `[${monsterEff[0]}]\n${monsterEff.slice(1).join("\n")}` : monsterEff[0],
-          pendulumEffect: effect[0]
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          console.error("Can't get data", error);
+          res.status(404).send(error.message);
         }
       } else {
-        effectResult = {
-          monsterEffect: effect.length > 1
-            ? (isExtraMonster ? `[${effect[0]}]\n${effect.slice(1).join("\n")}` : effect.join("\n"))
-            : effect[0]
-        };
-      }
-
-      const result = {
-        ...cardProperty,
-        effect: effectResult.monsterEffect,
-        pendulumEffect: effectResult.pendulumEffect,
-        cardSet: cardSetData.map(item => {
-          return {
-            releasedDate: item[0],
-            code: item[1],
-            name: item[2],
-            rarity: item[3],
-          }
-        }),
-        password: cardProperty.password === 'None' ? null : cardProperty.password,
-        isToken: cardProperty.password === 'None' && !!cardProperty.limitation_text
-      }
-
-      if (response) {
-        res.status(200).json(result);
-      } else {
-        console.error("Can't get data", error);
-        res.status(404).send(error.message);
+        console.error("Can't get page: ", response.statusMessage);
+        res.status(500).send('Cannot get data from Yugipedia');
       }
     });
   } catch (error) {
